b1d65e4ea89df8bc605fc66eb6297b5b1f1604d2
Jeffrey Paulouse <jeffreypaulousem24@gmail.com>
2025-06-19 15:47:44 +0530

    round 3

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+  let slideIndex = 1;
+  showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
+
+  function currentDiv(n) {
+    showDivs(slideIndex = n);
   }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
+
+  function showDivs(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].classList.remove("active");
+    }
+    slides[slideIndex - 1].style.display = "block";  
+    dots[slideIndex - 1].classList.add("active");
+  }
 
  const slides = document.querySelectorAll('.orbit-slide');
     const dots = document.querySelectorAll('.dot');
